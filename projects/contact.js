61c09c362127ab1a6e2609f73128f6b2011529f4
lauraen03 <lauraen@email.sc.edu>
2023-10-29 13:38:25 -0400

    json url

---
+++ projects/contact.js
@@ -0,0 +1,22 @@
+// contact.js
+
+// Function to populate the contact information
+async function populateContactInfo() {
+    try {
+        const response = await fetch('contact.json');
+        const data = await response.json();
+
+        const addressElement = document.getElementById('address');
+        const phoneElement = document.getElementById('phone');
+        const emailElement = document.getElementById('email');
+
+        addressElement.textContent = data.address;
+        phoneElement.textContent = data.phone;
+        emailElement.textContent = data.email;
+    } catch (error) {
+        console.error('Error fetching or parsing contact data:', error);
+    }
+}
+
+// Call the populateContactInfo function when the DOM is ready
+document.addEventListener('DOMContentLoaded', populateContactInfo);
