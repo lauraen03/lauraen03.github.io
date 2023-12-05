c2693348fcfe3c69a3600b23a73e09e82be5a8a6
lauraen03 <lauraen@email.sc.edu>
2023-12-05 17:47:21 -0500

    project

--- projects/contact.js
+++ projects/contact.js
@@ -18,6 +18,41 @@ async function populateContactInfo() {
         console.error('Error fetching or parsing contact data:', error);
     }
 }
+async function submitForm() {
+    const form = document.getElementById('contactForm');
+    const messages = document.getElementById('formMessages');
+
+    
+    messages.innerHTML = '';
+
+    
+    if (form.checkValidity()) {
+        try {
+            
+            const response = await fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                body: new FormData(form),
+            });
+
+            if (response.ok) {
+               
+                messages.innerHTML = 'Message sent successfully!';
+                form.reset();
+            } else {
+               
+                const result = await response.json();
+                messages.innerHTML = result.message;
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            messages.innerHTML = 'An unexpected error occurred. Please try again later.';
+        }
+    } else {
+       
+        form.reportValidity();
+    }
+}
+
 
 
 document.addEventListener('DOMContentLoaded', populateContactInfo);
