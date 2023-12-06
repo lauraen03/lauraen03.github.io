98a995c42f6ac790e98d23e014f357095613da62
lauraen03 <lauraen@email.sc.edu>
2023-12-06 11:57:40 -0500

    worked on project crud

--- projects/contact.js
+++ projects/contact.js
@@ -1,3 +1,68 @@
+const express = require('express');
+const Joi = require('joi');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost/bakery")
+  .then(() => console.log('Connected to mongodb'))
+  .catch(error => console.log("Couldn't connect to mongodb", error));
+
+const reviewSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  reviewText: String,
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+const reviewValidationSchema = Joi.object({
+  name: Joi.string().required(),
+  rating: Joi.number().required().min(1).max(5),
+  reviewText: Joi.string().required(),
+});
+
+// Route to get all reviews
+app.get('/api/reviews', async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.json(reviews);
+  } catch (error) {
+    console.error('Error fetching reviews:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Route to add a new review
+app.post('/api/add-review', async (req, res) => {
+  try {
+    const { name, rating, reviewText } = req.body;
+
+    // Validate the review data
+    await reviewValidationSchema.validateAsync({ name, rating, reviewText });
+
+    const newReview = new Review({ name, rating, reviewText });
+    const result = await newReview.save();
+    res.status(200).send('Review added successfully');
+  } catch (error) {
+    console.error('Error adding review:', error);
+    res.status(400).send('Error adding review');
+  }
+});
+
+// Add routes for updating and deleting reviews as needed
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+
 
 const jsonURL = 'https://lauraen03.github.io/assignments/Github/json/contact.json';
 
--- projects/review.js
+++ projects/review.js
@@ -1,52 +1,160 @@
-
 document.addEventListener('DOMContentLoaded', async function () {
-    const imageElement = document.querySelector('.image-section img');
-    
-    try {
-       
-        const response = await fetch('image.json');
-        
-        if (!response.ok) {
-            throw new Error('Failed to fetch image JSON');
-        }
-        
-        const data = await response.json();
-        const imagePath = data.imagePath;
-        
-      
-        imageElement.src = imagePath;
-    } catch (error) {
-        console.error('Error loading image:', error);
-    }
-    
-   
-});
+    // Fetch and display reviews
+    await fetchReviews();
 
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
     const reviewForm = document.getElementById('reviewForm');
     const customerReviews = document.querySelector('.customer-reviews');
 
-    reviewForm.addEventListener('submit', function (event) {
+    reviewForm.addEventListener('submit', async function (event) {
         event.preventDefault();
         const name = document.getElementById('name').value;
         const rating = document.getElementById('rating').value;
         const reviewText = document.getElementById('reviewText').value;
 
-        
-        const reviewElement = document.createElement('div');
-        reviewElement.className = 'review';
-        reviewElement.innerHTML = `
-            <p><strong>${name}</strong> (Rating: ${rating}):</p>
-            <p>${reviewText}</p>
-        `;
+        const reviewIdField = document.getElementById('reviewId');
+
+        if (reviewIdField.value) {
+          
+            const reviewId = reviewIdField.value;
 
         
-        customerReviews.appendChild(reviewElement);
+            await editReview(reviewId, { name, rating, reviewText });
 
-        
-        reviewForm.reset();
+           
+            await fetchReviews();
+
+            
+            reviewForm.reset();
+            reviewIdField.value = '';
+        } else {
+          
+            await addReview({ name, rating, reviewText });
+
+            await fetchReviews();
+
+           
+            reviewForm.reset();
+        }
+    });
+
+   
+    customerReviews.addEventListener('click', async function (event) {
+        if (event.target.classList.contains('edit-button')) {
+            const reviewId = event.target.dataset.reviewId;
+
+           
+            const newName = prompt('Enter new name:');
+            const newRating = prompt('Enter new rating (1-5):');
+            const newReviewText = prompt('Enter new review:');
+
+            
+            if (newName !== null && newRating !== null && newReviewText !== null) {
+                
+                await editReview(reviewId, { name: newName, rating: newRating, reviewText: newReviewText });
+
+              
+                await fetchReviews();
+            }
+        } else if (event.target.classList.contains('delete-button')) {
+            const reviewId = event.target.dataset.reviewId;
+
+          
+            await deleteReview(reviewId);
+
+            
+            await fetchReviews();
+        }
     });
 });
+
+
+async function getReviewById(reviewId) {
+    try {
+        const response = await fetch(`/api/reviews/${reviewId}`);
+        
+        if (!response.ok) {
+            throw new Error(`Error fetching review by ID: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching review by ID:', error);
+        return null; 
+    }
+}
+
+async function fetchReviews() {
+    try {
+        const response = await fetch('/api/reviews');
+        const data = await response.json();
+
+        const customerReviews = document.querySelector('.customer-reviews');
+        customerReviews.innerHTML = '';
+
+        data.forEach(review => {
+            const reviewElement = createReviewElement(review);
+            customerReviews.appendChild(reviewElement);
+        });
+    } catch (error) {
+        console.error('Error fetching reviews:', error);
+    }
+}
+
+async function addReview(reviewData) {
+    try {
+        await fetch('/api/reviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reviewData),
+        });
+    } catch (error) {
+        console.error('Error adding review:', error);
+    }
+}
+
+async function editReview(reviewId, reviewData) {
+    try {
+        const response = await fetch(`/api/reviews/${reviewId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reviewData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error editing review: ${response.statusText}`);
+        }
+
+        const updatedReview = await response.json();
+        return updatedReview;
+    } catch (error) {
+        console.error('Error editing review:', error);
+    }
+}
+
+async function deleteReview(reviewId) {
+    try {
+        await fetch(`/api/reviews/${reviewId}`, {
+            method: 'DELETE',
+        });
+    } catch (error) {
+        console.error('Error deleting review:', error);
+    }
+}
+
+function createReviewElement(review) {
+    const reviewElement = document.createElement('div');
+    reviewElement.className = 'review';
+    reviewElement.id = `review-${review._id}`;
+    reviewElement.innerHTML = `
+        <p><strong>${review.name}</strong> (Rating: ${review.rating}):</p>
+        <p>${review.reviewText}</p>
+        <button class="edit-button" data-review-id="${review._id}">Edit</button>
+        <button class="delete-button" data-review-id="${review._id}">Delete</button>
+    `;
+    return reviewElement;
+}
